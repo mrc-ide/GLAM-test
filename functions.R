72850ecc156a4fa267a71e8e50d67ecb8baa2020
Gina Cuomo-Dannenburg <g.cuomo-dannenburg18@imperial.ac.uk>
2025-04-08 14:35:37 +0100

    testing MCMC on simulated cohorts and functions to easily view output of one individual in a cohort

---
+++ functions.R
@@ -0,0 +1,47 @@
+# functions.R
+# 
+# Author: Gina Cuomo-Dannenburg
+# Date: 08/04/2025
+# 
+# Purpose: Functions to help with the testing and understanding of outputs of {glam}
+
+# load libraries
+library(tidyverse)
+library(GLAM)
+library(ggplot2)
+library(ggpubr)
+
+# filter a cohort to look at one individual and enable plotting
+filter_cohort <- function(cohort_sim, ind) {
+  return(cohort_sim$raw_list[[ind]])
+}
+
+# # test functions
+# # set simulation parameters
+# samp_time <- seq(0, 10, 1)
+# haplo_freqs <- rep(0.05, 20)
+# lambda <- 0.2
+# theta <- 2
+# decay_rate <- 0.1
+# sens <- 0.9
+# max_infections <- 20
+# cohort_size <- 10
+# 
+# # simulate cohort
+# sim <- sim_cohort(n = cohort_size,
+#                    samp_time = samp_time,
+#                    haplo_freqs = haplo_freqs,
+#                    lambda = lambda,
+#                    theta = theta,
+#                    decay_rate = decay_rate,
+#                    sens = sens,
+#                    n_inf = NULL,
+#                    return_full = TRUE)
+
+# ind1 <- filter_cohort(sim, 1)
+# plot_ind(ind1)
+# 
+# ind2 <- filter_cohort(sim, 2)
+# plot_ind(ind2)
+
+# plot MCMC posterior and individual data in one panel
--- test_mcmc.R
+++ test_mcmc.R
@@ -6,89 +6,94 @@
 # Purpose: test glam MCMC methods on simulated datasets
 
 # test how well the MCMC works on cohorts with the same n_inf
-sim <- readRDS("simulated_data/cohort_n_infections/cohort_1_infections.RDS")
+source("functions.R")
 
-# create a new MCMC object and load data
-g <- glam_mcmc$new(df_data = sim$df_data)
+n_inf <- seq(1:20)
 
-# initialize. If parameters are set to NULL, they are estimated. If they are
-# given a value, they take this fixed value. Useful for giving the inference
-# method the correct true value of some parameters, to diagnose how well it
-# estimates others.
-g$init(start_time = 0,
-       end_time = 10,
-       haplo_freqs = haplo_freqs,
-       lambda = lambda,
-       theta = theta,
-       decay_rate = decay_rate,
-       sens = sens,
-       n_infections = mapply(function(x) length(x$t_inf), sim$raw_list),
-       infection_times = NULL,
-       max_infections = 20,
-       chains = 1, rungs = 1)
+for(i in n_inf) {
+  print(paste("Performing MCMC on cohort with ", i, " infections"))
+  sim <- readRDS(paste0("simulated_data/cohort_n_infections/cohort_",i,"_infections.RDS"))
+  
+  # so it's easy to access cohorts afterwards
+  assign(paste0("n_",i), sim)
+  
+  # create a new MCMC object and load data
+  g <- glam_mcmc$new(df_data = sim$df_data)
+  
+  # initialize. If parameters are set to NULL, they are estimated. If they are
+  # given a value, they take this fixed value. Useful for giving the inference
+  # method the correct true value of some parameters, to diagnose how well it
+  # estimates others.
+  g$init(start_time = 0,
+         end_time = 10,
+         haplo_freqs = haplo_freqs,
+         lambda = lambda,
+         theta = theta,
+         decay_rate = decay_rate,
+         sens = sens,
+         n_infections = mapply(function(x) length(x$t_inf), sim$raw_list),
+         infection_times = NULL,
+         max_infections = 20,
+         chains = 1, rungs = 1)
+  
+  # run burn-in and sampling
+  g$burn(iterations = 1e3) |>
+    system.time()
+  
+  g$sample(iterations = 5e3) |>
+    system.time()
+  
+  # -----------------------
+  # EXTRACT OUTPUTS AND PLOT RESULTS
+  
+  # extract outputs into data.frames
+  df_global <- g$get_output_global()
+  df_n_infections <- g$get_output_n_infections()
+  df_infection_times <- g$get_output_infection_times()
+  
+  # dataframe of the times of infections in the simulated cohort
+  df_inf_time_true <- mapply(function(i) {
+    t <- sim$raw_list[[i]]$t_inf
+    if (length(t) == 0) {
+      return(NULL)
+    }
+    data.frame(individual = i,
+               infection = seq_along(t),
+               time = t)
+  }, seq_along(sim$raw_list), SIMPLIFY = FALSE) |>
+    bind_rows()
+  
+  # trace plot of a single infection time
+  df_infection_times |>
+    filter(infection == 1) |>
+    ggplot() + theme_bw() +
+    geom_point(aes(x = iteration, y = value, col = chain)) +
+    ylim(c(0, max_infections)) + facet_wrap(~individual)
+  ggsave(paste0("figures/cohort_mcmc/infection_chain_", i,".png"), dpi = 300)
+  
+  # density plot of all infection times
+  df_infection_times |>
+    filter(phase == "sampling") |>
+    group_by(individual, infection) |>
+    do({
+      d <- density(.$value)
+      data.frame(x = d$x, y = d$y)
+    }) |>
+    ungroup() |>
+    ggplot(aes(x = x, y = y, color = infection)) +
+    geom_vline(aes(xintercept = time), col = grey(0.5), 
+               data = df_inf_time_true) +
+    geom_line() +
+    facet_wrap(~ individual) +
+    labs(x = "Value", y = "Density", color = "Infection") +
+    theme_bw() +
+    theme(panel.grid.major = element_blank())
+  
+  ggsave(paste0("figures/cohort_mcmc/infection_time_", i,".png"), dpi = 300)
+}
 
-# run burn-in and sampling
-g$burn(iterations = 1e3) |>
-  system.time()
-
-g$sample(iterations = 5e3) |>
-  system.time()
-
-# -----------------------
-# EXTRACT OUTPUTS AND PLOT RESULTS
-
-# extract outputs into data.frames
-df_global <- g$get_output_global()
-df_n_infections <- g$get_output_n_infections()
-df_infection_times <- g$get_output_infection_times()
-
-# bivariate plots of scalar (global) parameters
-plot(df_global$decay_rate, df_global$sens, pch = 20, col = "#00000010", 
-     xlim = c(0, 0.5), ylim = c(0, 1))
-points(decay_rate, sens, pch = 4, cex = 2, col = 2, lwd = 3)
-
-plot(df_global$lambda, df_global$theta, pch = 20, col = "#00000010", 
-     xlim = c(0, 0.5), ylim = c(0, 2))
-points(lambda, theta, pch = 4, cex = 2, col = 2, lwd = 3)
-
-# dataframe of the times of infections in the simulated cohort
-df_inf_time_true <- mapply(function(i) {
-  t <- sim$raw_list[[i]]$t_inf
-  if (length(t) == 0) {
-    return(NULL)
-  }
-  data.frame(individual = i,
-             infection = seq_along(t),
-             time = t)
-}, seq_along(sim$raw_list), SIMPLIFY = FALSE) |>
-  bind_rows()
-
-# trace plot of a single infection time
-df_infection_times |>
-  filter(individual == 1) |>
-  filter(infection == 1) |>
-  ggplot() + theme_bw() +
-  geom_point(aes(x = iteration, y = value, col = chain)) +
-  ylim(c(0, max_infections))
-
-
-# density plot of all infection times
-df_infection_times |>
-  filter(phase == "sampling") |>
-  group_by(individual, infection) |>
-  do({
-    d <- density(.$value)
-    data.frame(x = d$x, y = d$y)
-  }) |>
-  ungroup() |>
-  ggplot(aes(x = x, y = y, color = infection)) +
-  geom_vline(aes(xintercept = time), col = grey(0.5), 
-             data = df_inf_time_true) +
-  geom_line() +
-  facet_wrap(~ individual) +
-  labs(x = "Value", y = "Density", color = "Infection") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank())
+# look at specific individuals to try and see why the MCMC is fitting the incorrect time of infection
+plot_ind(filter_cohort(sim, 7))
 
 
 
