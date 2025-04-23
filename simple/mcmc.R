858a816c1f88bfa5c3fa18e6204f9f4b31603775
Gina Cuomo-Dannenburg <g.cuomo-dannenburg18@imperial.ac.uk>
2025-04-23 14:24:22 +0100

    edits

--- simple/mcmc.R
+++ simple/mcmc.R
@@ -28,16 +28,18 @@ num_rungs <- 1
 # chosen parameters in alignment with the deploy script for the moment
 samp_time <- seq(0, 10, 1) # keep the same for all in this case
 haplo_freqs <- rep(0.05, 20) # equal frequency of all haplotypes
-theta_vec <- 2
-decay_vec <- 0.1
+theta_val <- 2
+decay_val <- 0.1
+lambda_val <- 1/10 # set to n_inf/max(samp_time)
 sens <- 1
 n_inf <- 1
 cohort_size <- 10
 
 # need to update when we have multiple samp_time to ensure that we filter by this
 sim_ids <- lookup |>
-  dplyr::filter(theta == theta_vec,
-                decay == decay_vec,
+  dplyr::filter(theta == theta_val,
+                decay == decay_val,
+                lambda == lambda_val,
                 sens == sens,
                 n == n_inf,
                 cohort_size == cohort_size) |>
@@ -46,6 +48,38 @@ sim_ids <- lookup |>
 for (i in 1:nrow(sim_ids)) {
   id <- sim_ids[i]
   sim <- readRDS(paste0("simple/data/sim", id, ".RDS"))
+  
+  # create a new MCMC object and load data
+  g <- glam_mcmc$new(df_data = sim$df_data)
+  
+  # initialize. If parameters are set to NULL, they are estimated. If they are
+  # given a value, they take this fixed value. Useful for giving the inference
+  # method the correct true value of some parameters, to diagnose how well it
+  # estimates others.
+  g$init(start_time = 0,
+         end_time = 10,
+         haplo_freqs = haplo_freqs,
+         theta = theta_val,
+         decay_rate = decay_val,
+         sens = sens,
+         n_infections = mapply(function(x) length(x$t_inf), sim$raw_list),
+         infection_times = NULL,
+         max_infections = 20,
+         chains = num_chains, rungs = num_rungs)
+  
+  # run burn-in and sampling
+  g$burn(iterations = burnin_iterations) |>
+    system.time()
+  
+  g$sample(iterations = sampling_iterations)  |>
+    system.time()
+  
+  # save output
+  saveRDS(g, paste0("simple/mcmc_outputs/out", id,".RDS"))
+  
+  # summary outputs
+  
+  
 }
 
 
