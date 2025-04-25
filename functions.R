b610fea7e47b80cc625d460ff28d8c8c4722f290
Gina Cuomo-Dannenburg <g.cuomo-dannenburg18@imperial.ac.uk>
2025-04-25 11:34:28 +0100

    removed functions that enabled functionality now included in new {glam} version

--- functions.R
+++ functions.R
@@ -11,61 +11,6 @@ library(GLAM)
 library(ggplot2)
 library(ggpubr)
 
-# filter a cohort to look at one individual and enable plotting
-filter_cohort <- function(cohort_sim, ind) {
-  return(cohort_sim$raw_list[[ind]])
-}
-
-# plot an individual without information on when a bite was and without the 
-# information on what haplotypes were introduced 
-# seeing the individuala as the MCMC would
-
-mcmc_sees <- function (ind) {
-  start <- haplo <- end <- time <- NULL
-  # this throws an error despite it being in plot_ind and that rendering?!
-  # assert_in(c("state_obs", "t_inf", "w_init", "clear_init", 
-  #             "w_inf", "clear_inf", "state_true", "samp_time"), names(ind))
-  state_obs <- ind$state_obs
-  t_inf <- ind$t_inf
-  w_init <- ind$w_init
-  clear_init <- ind$clear_init
-  w_inf <- ind$w_inf
-  clear_inf <- ind$clear_inf
-  state_true <- ind$state_true
-  samp_time <- ind$samp_time
-  n_haplo <- length(w_init)
-  n_samp <- length(samp_time)
-  start_time <- samp_time[1]
-  end_time <- samp_time[n_samp]
-  n_inf <- length(t_inf)
-  df_plot <- data.frame(start = rep(start_time, sum(w_init)), 
-                        end = clear_init[w_init == 1], haplo = which(w_init == 
-                                                                       1))
-  if (n_inf > 0) {
-    df_plot <- bind_rows(df_plot, data.frame(start = (w_inf * 
-                                                        t_inf)[w_inf == 1], end = clear_inf[w_inf == 1], 
-                                             haplo = which(w_inf == 1, arr.ind = TRUE)[, 2]))
-  }
-  
-  df_plot <- mutate(data.frame(time = samp_time, 
-                               haplo = rep(1:n_haplo, each = n_samp), 
-                               state_true = c("Absent", "Present")[state_true + 1], 
-                               state_obs = c("Unobserved", "Observed")[state_obs + 1]), 
-                    state_true = factor(state_true, levels = c("Absent",  "Present")), 
-                    state_obs = factor(state_obs, levels = c("Unobserved", "Observed"))) |>
-    dplyr::mutate(positive = if_else(state_true == "Present" & state_obs == "Observed", "Positive", "Negative"),
-                  positive = factor(positive, levels = c("Negative","Positive")))
-  ggplot(df_plot) + theme_bw() + 
-    geom_point(aes(x = time, y = haplo, alpha = positive, col = positive), size = 3, data = df_plot) + 
-    scale_color_discrete(name = "Test result") + 
-    scale_alpha_manual(values = c(0.1, 1), name = "Test result") +
-    xlab("Time") + 
-    ylab("Haplotype") + 
-    xlim(c(start_time, end_time)) + 
-    ylim(c(1, n_haplo)) + 
-    theme(panel.grid = element_blank())
-}
-
 # Function to zero-pad integer ensuring fixed length
 zero_pad_fixed <- function(num, places) {
   sprintf(paste0("%0", places, "d"), as.integer(num))
