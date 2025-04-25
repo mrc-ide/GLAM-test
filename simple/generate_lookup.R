e655c5754be8a2df580c9638ab7890a7955820bd
Gina Cuomo-Dannenburg <g.cuomo-dannenburg18@imperial.ac.uk>
2025-04-25 14:46:52 +0100

    R script to enable generating the lookup table so that I can source from cluster if necessary

---
+++ simple/generate_lookup.R
@@ -0,0 +1,36 @@
+# simple/simulate_function.R 
+# Author: Gina Cuomo-Dannenburg
+# Date: 2025-04-25
+# Purpose: Generate the lookup table - included here for ease of duplicating for 
+# banks of simulations and to allow it to also be run on the cluster
+
+# add this to hipercow environment
+source("functions.R")
+
+# define the vector of options that we want to cycle over for the simulations
+samp_time <- seq(0, 10, 1) # keep the same for all in this case
+haplo_freqs <- rep(0.05, 20) # equal frequency of all haplotypes
+theta_vec <- c(seq(0.1, 0.5, 0.1), seq(1, 5, 0.5))
+decay_vec <- c(seq(0.1, 1, 0.1))
+sens <- 1
+n_inf <- 1:10
+cohort_size <- 10
+repetitions <- 100 # set repetitions to be 100 so we can test how often MCMC 
+# returns true parameters
+
+# generate a lookup table with all the parameter values
+lookup <- 
+  # start with cycling over the vectors
+  tidyr::expand_grid(theta = theta_vec, 
+                     decay = decay_vec, 
+                     n = n_inf,
+                     repetition = 1:repetitions)|>
+  dplyr::mutate(lambda = n/max(samp_time),
+                samp_time = list(samp_time),
+                haplo_freqs = list(haplo_freqs),
+                sens = sens,
+                cohort_size = cohort_size) |>
+  dplyr::select(cohort_size, samp_time, haplo_freqs, lambda,
+                theta, decay, sens, n, repetition) |>
+  dplyr::mutate(sim_id = zero_pad_fixed(row_number(), 9)) # generate a simulation id number
+saveRDS(lookup, "simple/lookup.RDS")
