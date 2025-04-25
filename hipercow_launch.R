45a677b859a09df0ac7d6d269129bc6e9126fa5f
Gina Cuomo-Dannenburg <g.cuomo-dannenburg18@imperial.ac.uk>
2025-04-25 16:27:46 +0100

    changes based on debugging the cluster integration

--- hipercow_launch.R
+++ hipercow_launch.R
@@ -1,15 +1,16 @@
 library(hipercow)
 hipercow_init()
-hipercow_configure(driver = "windows")
+hipercow_configure(driver = "dide-windows")
 hipercow_provision(method = "pkgdepends")
 hipercow_environment_create(sources = c("simple/simulate_function.R", 
-                                        "mcmc_function.R"))
-hipercow_resources(cores = 20)
+                                        "simple/mcmc_function.R"))
+hipercow_resources(cores = 1)
 
 lookup <- readRDS("simple/lookup.RDS")
-rows <- 1:nrow(lookup)
+rows <- 1:2 #nrow(lookup)
 
-bundle <- task_create_bulk_call(simulate_data, rows, 
+bundle <- task_create_bulk_call(fn = simulate_data, 
+                                data = rows, 
                                 bundle_name = "simulate-data")
 bundle <- task_create_bulk_call(mcmc_fitting, rows, 
                                 bundle_name = "fit-simulated-data")
